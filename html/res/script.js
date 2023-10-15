c950d59e14f11e723d075de33ce58459adbe9f83
Alejandro Alzate <alejandroalzatesanchez@gmail.com>
2023-10-15 12:25:35 -0500

    v1.0

---
+++ html/res/script.js
@@ -0,0 +1,20 @@
+function luckButton() {
+	document.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
+
+function searchButton() {
+	var irlGoogle = 'https://www.google.com/search?q="';
+	var query = document.getElementById('search-bar-textarea').value;
+	if (query != "") {
+		document.location = irlGoogle + query + '"';
+	} else {
+		return false;
+	}
+}
+
+function fixCountryPos() {
+	setTimeout(function() {
+	document.getElementById('country-text').style['top'] = "100%";
+}, 10);}
+
+fixCountryPos()
